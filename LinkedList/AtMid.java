a026fcc250498722602f81ece9d22218fa627245
Shrutika19 <jaiswalshrutika19@gmail.com>
2021-09-16 22:02:49 +0530

    Modified function

--- LinkedList/AtMid.java
+++ LinkedList/AtMid.java
@@ -6,78 +6,69 @@ class Node{
 }
 class List{
 	Node head  = null;
-	Node newnode, temp;
+	Node newnode, temp, nextnode;
 	public int count = 0;
 	int pos, i;
 	Scanner scan  = new Scanner(System.in);
 
-	public void add(int x){
-
-		if(head == null){
-			newnode = new Node();
+	 public void add(int x){
+        		if(head == null){
+            			newnode = new Node();
 			newnode.data = x;
 			count++;
 			System.out.println("Number of nodes: " +count);
 			newnode.next = null;
 			head = newnode;
-		}else{
-			System.out.println("Enter location to insert node:");
-			pos = scan.nextInt();
-
-			if( pos > count){
-				System.out.println("Enter valid location!");
-			}else{
-				newnode = new Node();
-				newnode.data = x;
-				count++;
-				System.out.println("Number of nodes: " +count);
-
-				temp = head;
-				for(i = 1; i < pos - 1; i++){
-					temp = temp.next;
-				}
-				
-				newnode.next = temp.next;
-				temp.next = newnode;
-			}
-			
-		}
-	}
+       		 }else{
+            			System.out.println("Enter location to insert node:");
+            			pos = scan.nextInt();
+            
+            		if(pos >= count){
+                			System.out.println("Enter valid location!");
+            		}else{
+                			temp = head;
+                
+               			 for(int i = 1; i < pos-1; i++){
+                    			temp = temp.next;
+                			}
+                
+                		newnode = new Node();
+                		newnode.data = x;
+                		newnode.next = temp.next;
+               		temp.next = newnode;
+                		count++;
+                		System.out.println("Total number of nodes: " +count);
+            }
+        }
+    }	
 
 	public void remove(){
-		int pos,i = 1;
-		Node temp, nextnode;
+        		if(head == null){
+            			System.out.println("Linked list is empty!");
+        		}else{
+            			System.out.println("Enter location to delete node:");
+           			pos = scan.nextInt();
+            
+            		if(pos > count){
+                			System.out.println("Enter valid location!");
+            		}else{
+                			temp = head;
+                
+                			for(int i = 1; i < pos-1; i++){
+                    			temp = temp.next;
+                			}
+                
+                nextnode = temp.next;
+                temp.next = nextnode.next;
+                count--;
+                System.out.println("Node is deleted!");
+            }
+        }
+    }
+
+	
 		
-		if(head == null){
-			System.out.println("Linked list is empty!");
-		}else if(count == 1){
-			temp = head;
-			head  = head.next;
-			count--;
-			System.out.println("Node is deleted!");
-		}else{
-			System.out.println("Enter location to delete node:");
-			pos = scan.nextInt();	
 
-			if(pos > count){
-				System.out.println("Enter valid location!");
-			}else{
-				temp = head;
-
-				while(i < pos - 1){
-					temp = temp.next;
-					i++;
-				}
-				nextnode = temp.next;
-                			temp.next = nextnode.next;
-                			count--;
-                			System.out.println("Node is deleted!");
-			}
-
-
-		}
-		
-	}
 
 	public void show(){
 		if(head == null){
